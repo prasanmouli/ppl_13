596d7e17cd8347fb50592a0d6d263e3f38446338
prasanmouli <prasanmouli@gmail.com>
2013-01-04 21:18:58 +0530

    Update pages/login.php

--- pages/login.php
+++ pages/login.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php
-	include("config.lib.php");
-?>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-<?php
+include("config.lib.php");
 session_start();
 $flag = 0;
 $email = addslashes($_POST['email']);
@@ -31,6 +23,3 @@ if($flag!=1){
 	header("location: .././");
 	}
 ?>
-<body>
-</body>
-</html>
